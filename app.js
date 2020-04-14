be360d9dba8e31dde125f7f1ab25907d1d8c19bf
Francis Bukenya <frankie.bukenya@gmail.com>
2020-04-14 13:44:39 +0200

    Add v.0.x

---
+++ app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/index'));
+
+module.exports = app;
