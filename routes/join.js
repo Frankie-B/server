da28c8c80983e5c3a5130952076101814e41f52f
Piepognwong <j.tonneyck@gmail.com>
2020-04-21 21:34:11 +0200

    shared app

--- routes/join.js
+++ routes/join.js
@@ -1,13 +1,12 @@
 const http = require('http');
-const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
+const app = require("../app.js");
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const router = require('./router');
 
-const app = express();
 const server = http.createServer(app);
 
 const PORT = process.env.SOCKET_PORT || 5000;
